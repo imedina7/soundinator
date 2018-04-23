2a2768cf498bdcd03c4ed40e41cd6e12175e38fd
Israel Medina <israel.medina@salesforce.com>
2018-04-23 19:02:24 -0300

    added frontend support

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,34 +56,53 @@ function b64toBlob(b64Data, contentType, sliceSize) {
 
 
 ( function () {
-  
   var sounds = new Array();
-  var fdata = new FormData();
-  fdata.append("SESSION_ID","jhhgbhvfyt67gjbygyu");
-  
-  var req = new Request("/api.php",{ method: "POST", body: fdata});
-  fetch(req).then(function (response){
-    return response.json();
+  if (localStorage.getItem("session_id") != null){
     
-  }).then(function (json_response) {
-      var soundList = json_response.soundList;
-      soundList.forEach(function (element) {
-        var currentSound = new Sound(element.name);
-        currentSound.id = element.id;
-        currentSound.blob = b64toBlob(element.data,element.contentType,1024);
-        currentSound.blobUrl = URL.createObjectURL(currentSound.blob);
-        sounds.push(currentSound);
-      })
+    var fdata = new FormData();
+    fdata.append("SESSION_ID",localStorage.getItem("session_id"));
+
+    var req = new Request("/api.php",{ method: "POST", body: fdata});
+    fetch(req).then(function (response){
+      return response.json();
       
-  }).catch(function(error){
-      console.log("no funcionó: "+ error);
-  });
-  
+    }).then(function (json_response) {
+        var soundList = json_response.soundList;
+        soundList.forEach(function (element) {
+          var currentSound = new Sound(element.name);
+          currentSound.id = element.id;
+          currentSound.blob = b64toBlob(element.data,element.contentType,1024);
+          currentSound.blobUrl = URL.createObjectURL(currentSound.blob);
+          sounds.push(currentSound);
+        })
+        
+    }).catch(function(error){
+        console.log("no funcionó: "+ error);
+    });
+  }
   const App = new Vue({
     el: "#app",
     data: {
+      loggedIn: false,
+      loginform: false,
       title: "Welcome!",
       soundList: sounds
+    },
+    methods: {
+      onSubmit: function(event){
+        var fdata = new FormData(document.getElementById("loginForm"));
+    
+        var req = new Request("/login.php",{ method: "POST", body: fdata});
+        fetch(req).then(function (response){
+          return response.json();
+          
+        }).then(function (json_response) {
+            localStorage.setItem("session_id", json_response.session_id);
+            console.log("session_id = ")
+        }).catch(function(error){
+            console.log("no funcionó: "+ error);
+        });
+      }
     }
   });
 })();
