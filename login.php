2aabcf99e9f363b73b65a18d9e788431f35a0ed8
Israel Medina <israel.medina@salesforce.com>
2018-04-23 20:24:23 -0300

    added debug log to login.php

--- login.php
+++ login.php
@@ -6,9 +6,9 @@ header("Content-type: application/json; charset=utf-8");
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
 
-    if ($session_id = Auth::validate_user($_POST['username'],$_POST['password']))
-
-        echo '{ "session_id": "'.$session_id.'"}';
-    else
+    if ($session_id = Auth::validate_user($_POST['username'],$_POST['password'])) {
+        error_log('sent response: ' . '{ "session_id": "'.$session_id.'" }');
+        echo '{ "session_id": "'.$session_id.'" }';
+    } else
         echo '{ "error": "Invalid username and/or password" }';
 }
