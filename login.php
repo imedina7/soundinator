b2ae8ed052ace7559de5de8c6e83e8b6a990ac2d
Israel Medina <israel.medina@salesforce.com>
2018-04-23 20:28:21 -0300

    fix

--- login.php
+++ login.php
@@ -9,6 +9,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     if ($session_id = Auth::validate_user($_POST['username'],$_POST['password'])) {
         $outObj = Array ("session_id" => $session_id);
         $jsonOutput = json_encode($outObj);
+        error_log($jsonOutput);
         echo $jsonOutput;
     } else
         echo '{ "error": "Invalid username and/or password" }';
