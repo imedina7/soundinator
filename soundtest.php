5caf17428627a0a92f280757c60fb628ee9fbc22
Israel Medina <israel.medina@salesforce.com>
2018-04-24 13:59:45 -0300

    no message

--- soundtest.php
+++ soundtest.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/lib/clients/DatabaseConnect.php";
 
 $db = DatabaseConnect::getInstance();
-$s = $db->getSound(2);
+$s = $db->getSound(3);
 
 header("Content-type: ".$s['sound_type']."; charset=utf-8");
 echo base64_decode($s['data_base64']);
