e2a6363de64a7aa76df256c0767981b4361886fa
Israel Medina <israel.medina@salesforce.com>
2018-04-24 19:33:49 -0300

    changed titile

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Soundinator - The Official Botonera for The πbes</title>
+    <title>Soundinator - The Real Botonera for The πbes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
     <link rel="stylesheet" type="text/css" media="screen" href="assets/css/normalize.css" />
