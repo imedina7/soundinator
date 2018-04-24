0621af51618a5c8d0c54856d60e4b7b03e4b9e2d
Israel Medina <israel.medina@salesforce.com>
2018-04-23 22:23:33 -0300

    added libraries

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
             <input type="submit" name="submit" id="submit" value="Login">
           </form>
         </div>
-        <div v-if="loggedIn">So you are logged in, you can always <a href="#" @click="logout()">log out</a></div>
+        <p class="lead" v-if="loggedIn">So you are logged in, you can always <a href="#" @click="logout()">log out</a></p>
         <ul>
           <li v-for="sound in soundList">
             <a href="#" class="soundButton" :data-sound-id="sound.id" v-bind:class="{ 'playing' : sound.isplaying }" v-on:click="sound.play()">{{sound.name}}</a>
@@ -61,5 +61,8 @@
     </div>
   </div>
   <script src="assets/js/main.js"></script>
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 </body>
 </html>
