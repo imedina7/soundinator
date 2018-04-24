5f2a9d954b54e8663a195d00016bc5835f3481ef
Israel Medina <israel.medina@salesforce.com>
2018-04-23 23:40:46 -0300

    styling

--- index.php
+++ index.php
@@ -19,20 +19,22 @@
 <body>
   <div id="app" class="principal">
     <h1>{{ title }}</h1>
-    <div v-if="! loggedIn">
-      <p class="lead">Looks like you are not logged in! <a href="#" @click="loginform = !loginform">Log in</a> first to see your sounds and add new ones.</p>
-      <form v-if="loginform" id="loginForm" v-on:submit.prevent="onSubmit">
-        <input type="text" name="username" id="username" placeholder="Username...">
-        <input type="password" name="password" id="password" placeholder="Password...">
-        <input type="submit" name="submit" id="submit" value="Login">
-      </form>
+    <div class="columnas">
+      <div v-if="! loggedIn">
+        <p class="lead">Looks like you are not logged in! <a href="#" @click="loginform = !loginform">Log in</a> first to see your sounds and add new ones.</p>
+        <form v-if="loginform" id="loginForm" v-on:submit.prevent="onSubmit">
+          <input type="text" name="username" id="username" placeholder="Username...">
+          <input type="password" name="password" id="password" placeholder="Password...">
+          <input type="submit" name="submit" id="submit" value="Login">
+        </form>
+      </div>
+      <p class="lead" v-if="loggedIn">So you are logged in, you can always <a href="#" @click="logout()">log out</a></p>
+      <ul class="columnas soundList">
+        <li v-for="sound in soundList">
+          <a href="#" class="soundButton" :data-sound-id="sound.id" v-bind:class="{ 'playing' : sound.isplaying }" v-on:click="sound.play()">{{sound.name}}</a>
+        </li>
+      </ul>
     </div>
-    <p class="lead" v-if="loggedIn">So you are logged in, you can always <a href="#" @click="logout()">log out</a></p>
-    <ul>
-      <li v-for="sound in soundList">
-        <a href="#" class="soundButton" :data-sound-id="sound.id" v-bind:class="{ 'playing' : sound.isplaying }" v-on:click="sound.play()">{{sound.name}}</a>
-      </li>
-    </ul>
   </div>
   <script src="assets/js/main.js"></script>
 </body>
